2375bb2344c16774dc89ca5d848de6896da94d2b
Kyle Grosser <kylefg95@gmail.com>
2021-12-08 14:00:56 -0500

    Create Intercept_in_DGM.R
    what happens when the DGM has a non-zero intercept?

---
+++ Intercept_in_DGM.R
@@ -0,0 +1,70 @@
+library(impeRfect)
+library(lme4)
+library(geex)
+library(tidyverse)
+set.seed(95)
+
+# number of simulations; number of clusters; obs. per cluster
+n.sims = 50; n = 1000; m = 3
+# vector of fixed effects
+beta = c(-1, 2, -1)
+p = length(beta)
+p.X = p - 1
+# random error sd; random errors
+sigma = 1.2
+
+data.list = list()
+for (i in 1:n.sims) { 
+  # when b is NULL, random intercept is generated from N(0, 1)
+  b = NULL
+  # otherwise, pre-define b as an n-dimensional vector of random intercepts
+  # b = rt(n = n, df = 3), for example
+  data.list[[i]] = generate_data(n = n, m = m, b = b, p.X = p.X, p.A = 0, 
+                                 beta = beta, sigma = sigma) 
+}
+
+lme.results = matrix(data = NA, nrow = n.sims, ncol = p + 1)
+ee.results = matrix(data = NA, nrow = n.sims, ncol = p)
+
+for (i in 1:n.sims) {
+  print(paste("Sim",i, "starting"))
+  sim.data = data.list[[i]]
+  # use lmer() to fit linear mixed effects model
+  lme.fit = lmer(formula = Y ~ X1 + X2 + (1 | id), data = sim.data) %>%
+    summary()
+  lme.results[i, ] = c(coef(lme.fit)[, 1], lme.fit$sigma^2)
+  # lme.results[i, 5:7] = sqrt(diag(vcov(lme.fit)))
+  
+  # use m_estimate() to solve estimating equations defined above
+  ee.fit = m_estimate(estFUN = eff_score_vec, 
+                      data = sim.data, units = "id",
+                      root_control = setup_root_control(start = lme.results[i, 2:(p+1)]),
+                      outer_args = list(response = "Y", X.names = paste0("X", 1:(p - 1))))
+  
+  ee.results[i, ] = coef(ee.fit)
+  print(paste("Sim",i, "complete"))
+  # ee.results[i, 5:7] = sqrt(diag(vcov(ee.fit))[1:3])
+}
+
+c(beta, sigma^2)
+colMeans(lme.results)
+colMeans(ee.results)
+
+sqrt(diag(vcov(ee.fit)))
+
+plot.df = ee.results %>%
+  as.data.frame()
+colnames(plot.df) = c("beta1", "beta2", "sigma2")
+plot.df = plot.df %>%
+  mutate(beta1 = beta1 - beta[2], beta2 = beta2 - beta[3], sigma2 = sigma2 - sigma^2) %>%
+  gather(key = "parameter", value = "estimate")
+head(plot.df)
+
+p1 <- plot.df %>%
+  # filter(parameter == "beta1") %>%
+  ggplot(aes(x = parameter, y = estimate, fill = parameter)) + 
+  geom_boxplot() +
+  # scale_y_continuous(breaks = seq(-0.15, 0.1, 0.05)) +
+  ggtitle("Empirical bias (estimate - truth) for 50 simulations") +
+  theme_bw()
+p1
