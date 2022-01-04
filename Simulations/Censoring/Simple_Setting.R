f4651b65d08629b3d88bcc7669914870662644d8
Kyle Grosser <kylefg95@gmail.com>
2022-01-04 10:38:18 -0500

    added code for estimation with eff_score_vec

--- Simulations/Censoring/Simple_Setting.R
+++ Simulations/Censoring/Simple_Setting.R
@@ -1,6 +1,8 @@
 # need this for simulation of Cox model outcome
 devtools::install_github(repo = "Tanya-Garcia-Lab/Imputing-Censored-Covariates", subdir = "imputeCensoRd")
-# library(tidyverse)
+library(lme4)
+library(geex)
+
 
 set.seed(114)
 # number of subjects
@@ -23,6 +25,9 @@ long.data = DGM_2(n = n, m = m, b = NULL,
                   logHR = logHR, beta = beta, 
                   alpha = alpha, sigma = sigma)
 
+# create time_since_event variable, age - t
+long.data$time_since_event = with(long.data, age - t)
+
 # inspect data frame
 head(long.data)
 
@@ -32,7 +37,7 @@ head(long.data)
 # sanity check with lm()
 # truth = (0.5, 2, -1, 1.5)
 # NOTE: I use the covarirate (age - t), which correctly follows the DGM
-lm(formula = y ~ z_y1 + z_y2 + I(age - t), data = long.data) %>% coef()
+lm(formula = y ~ z_y1 + z_y2 + time_since_event, data = long.data) %>% coef()
 
 # sanity check with coxph()
 # truth = (1, -0.5)
@@ -42,3 +47,22 @@ survival::coxph(formula = survival::Surv(t, delta) ~ z_t1 + z_t2, data = long.da
 ## NEXT STEPS:
 # impute using imputeCensoRd::condl_mean_impute()
 # analyze with impeRfect::eff_score_vector()
+
+
+# get initial parameter estimates with lmer()
+lme.fit = lmer(formula = y ~ z_y1 + z_y2 + time_since_event + (1 | id) - 1, data = long.data)
+lme.sum = summary(lme.fit)
+lme.est = c(coef(lme.sum)[, 1], lme.sum$sigma^2)
+lme.est
+
+# use m_estimate() to solve estimating equations defined above
+ee.fit = m_estimate(estFUN = eff_score_vec, 
+                    data = long.data, units = "id",
+                    root_control = setup_root_control(start = lme.est),
+                    outer_args = list(response = "y", 
+                                      X.names = c("z_y1", "z_y2", "time_since_event"),
+                                      cens = "delta"))
+
+# truth = c(2, -1, 1.5, 1)
+coef(ee.fit)
+
