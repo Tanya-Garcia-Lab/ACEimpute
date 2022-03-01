8bf69833c67b41a9dc36dbaf23eedf4a3855f3fb
Kyle Grosser <kylefg95@gmail.com>
2022-03-01 14:11:23 -0500

    Updated to allow for random slopes

--- impeRfect/R/eff_score_vector.R
+++ impeRfect/R/eff_score_vector.R
@@ -4,50 +4,53 @@
 #' Estimates the parameters of a linear mixed model when we do
 #' not want to specify a distribution for the random effects and
 #' when one covariate is (possibly) subject to random right-censoring
-#' Takes in dataframe 'data', which corresponds to data from a
+#' Takes in dataframe \code{data}, which corresponds to data from a
 #' single cluster, then returns the value of efficient score vector for
 #' theta = (beta, sigma^2) corresponding to that cluster.
-#' Intended for use with m_estimate() function
-#' At the moment, this function only allows for random intercept.
+#' Intended for use with m_estimate() function.
 #'
 #' @param data dataframe for one cluster of observations
 #' @param response character denoting the column name for the model response
 #' @param X.names string of characters denoting the columns for the model covariates that are associated with fixed effects
-#' @param cens character denoting the column name for the censoring indicator, which is 1 if not censored and 0 if censored.
-#' Default value is NULL, in which case the cluster is considered not censored.
 #' @param Z.names string of characters denoting the columns for the model covariates that are associated with random effects.
 #' Default value is NULL, in which case the random effects design matrix is assumed to be a column of 1's
+#' @param cens character denoting the column name for the censoring indicator, which is 1 if not censored and 0 if censored.
+#' Default value is NULL, in which case the cluster is considered not censored.
 #'
 #' @importFrom MASS ginv
 #'
 #' @export
 eff_score_vec = function(data, response,
                          X.names, Z.names = NULL, cens = NULL) {
+
   # number of observations in cluster
   m = nrow(data)
 
   # cens.bool = 1 if cens != NULL and data[1, cens] == 0
   cens.bool = !is.null(cens)
-  if (cens.bool) { cens.bool = (data[1, cens] == 0) }
+  if (cens.bool) { cens.bool = I(data[1, cens] == 0) }
 
   # number of fixed effects
   p = length(X.names)
-  # number of random effects
-  q = 1 + length(Z.names)
 
   # longitudinal response
   y = as.matrix(data[, response])
   # fixed effects design matrix
   X = as.matrix(data[, X.names])
+
   # random effects design matrix
   if (is.null(Z.names)) {
     # if Z.names not provided, Z = 1_m
     Z = matrix(1, m, 1)
-  }
-  else {
-    # otherwise, use the design matrix specified by Z.names
+    # number of random effects is 1
+    q = 1
+  } else {
+    # otherwise, use random effects design matrix specified by Z.names
     Z = as.matrix(data[, Z.names])
+    # number of random effects is number of columns specified by Z.names
+    q = length(Z.names)
   }
+
   # add leading column of 1_m if censored
   if (cens.bool) {
     Z = cbind(1, Z)
