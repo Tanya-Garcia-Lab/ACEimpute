83eedda4bf3371297fdaaca7def5a92bda1a8f54
Kyle Grosser <kylefg95@gmail.com>
2023-02-15 11:31:00 -0500

    Added Oracle models

---
+++ Manuscripts_Simulations/Correctly_Specified_Imputation_Model/R_scripts/full_data_reml_analysis.R
@@ -0,0 +1,73 @@
+# clear workspace
+rm(list = ls())
+
+# set working and library directories
+setwd("~/Documents/GitHub/ACEimpute/Manuscripts_Simulations/")
+
+# Load packages
+library(tidyverse)
+library(lme4)
+
+# read in simulated datasets
+simulated_datasets = readr::read_rds("simulated_data/simulated_datasets_25perc_cens.rds")
+
+# number of simulations
+max_sim <- max(simulated_datasets$replicate)
+num_sim = max_sim
+
+param_names = c("beta1", "alpha", "sigma2")
+n_param = length(param_names)
+method_names = c("fd_reml")
+n_method = length(method_names)
+
+save_res = matrix(data = NA, 
+                  nrow = num_sim, 
+                  ncol = 2 + 2*n_param*n_method) %>%
+  as.data.frame()
+
+colnames(save_res) = c("sim", "censored", paste0(rep(paste0(c("", "se_"), rep(param_names, each = 2)), n_method), 
+                                                 "_",
+                                                 rep(method_names, each = 2*n_param)))
+
+save_res$sim = 1:num_sim
+
+head(save_res)
+
+for (s in 1:num_sim) {
+  ## Generate longitudinal data with 1 random slope per subject and no random intercept
+  data_s <- simulated_datasets %>%
+    filter(replicate == s)
+  
+  ## DATASET 1: use REML when full data is available
+  # fit mixed effects model with lmer()
+  fd_reml_sum <- lmer(formula = y ~ x1 + time_to_event + (z1 - 1 | id) - 1, 
+                      data = data_s, REML = T) %>%
+    summary()
+  
+  # save parameter estimates
+  save_res[s, paste0(param_names, "_fd_reml")] <- c(coef(fd_reml_sum)[, 1], fd_reml_sum$sigma^2)
+  # save standard error estimates
+  save_res[s, paste0("se_", param_names, "_fd_reml")] <- c(sqrt(diag(fd_reml_sum$vcov)), NA)
+  
+  # ## DATASET 2: use REML with complete cases only
+  # # use complete case analysis
+  # cc_reml_sum <- data_s %>%
+  #   dplyr::filter(delta == 1) %>%
+  #   lmer(formula = y ~ x1 + time_to_event + (z1 - 1 | id) - 1, 
+  #        data = .) %>%
+  #   summary()
+  # 
+  # # save parameter estimates
+  # save_res[s, paste0(param_names, "_cc_reml")] <- c(coef(cc_reml_sum)[, 1], cc_reml_sum$sigma^2)
+  # # save standard error estimates
+  # save_res[s,  paste0("se_", param_names, "_cc_reml")] <- c(sqrt(diag(cc_reml_sum$vcov)), NA)
+  # 
+  if (s %% 25 == 0) print(paste("Simulation", s, "complete! :D"))
+  
+  write.csv(x = save_res, file = "~/ACE_sims/sim_results/fd_reml_estimates.csv", row.names = F)
+}
+
+# read.csv("sim_results/fd_reml_estimates.csv") %>%
+#   colMeans()
+
+# write.csv(x = save_res, file = "fd_reml_estimates.csv")
